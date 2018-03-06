46e236016cf5cc63c114c257bb239cc25c009a30
Matthias Koenig <konigmatt@googlemail.com>
2018-03-06 11:48:28 +0100

    tutorial for virtual environments

--- talks/02_virtualenv/simulation.py
+++ talks/02_virtualenv/simulation.py
@@ -2,21 +2,84 @@
 Simple ODE model demonstrating virtual environments.
 """
 
+import numpy as np
 import pandas as pd
-import tellurium as te
+from scipy.integrate import odeint
 from matplotlib import pyplot as plt
 
+# global settings for plots
+plt.rcParams.update({
+        'axes.labelsize': 'large',
+        'axes.labelweight': 'bold',
+        'axes.titlesize': 'large',
+        'axes.titleweight': 'bold',
+        'legend.fontsize': 'large',
+        'xtick.labelsize': 'large',
+        'ytick.labelsize': 'large',
+        'figure.facecolor': '1.00'
+    })
 
-# Integrate ODE system
-r = te.loada("""
-    J0: X1 -> X2; k1*X1;
-    X1 = 10.0; X2 = 0.0; k1 = 0.1;
-""")
-s = r.simulate(start=0, end=10, steps=200)
-s = pd.DataFrame(s, columns=s.colnames)
+
+def lorenz_dxdt(x, t):
+    """ Differential equations for Lorenz.
+
+    :param x: state vector
+    :param t: time
+    :return: ode system
+    """
+    r = 45.92
+    b = 4.0
+    sig = 16.0
+    return [
+        sig*(x[1] - x[0]),
+        -x[0]*x[2] + r*x[0] - x[1],
+        x[0]*x[1] - b*x[2],
+    ]
 
 
-fig, ax = plt.subplots(nrow=1, ncol=1)
-ax.plot(s.time, s.X1)
-ax.plot(s.time, s.X2)
-plt.show()
+def lorenz_plot(s):
+    """ Plot the Lorenz attractor.
+
+    :param s: Data Frame with results
+    :return:
+    """
+    # plot results
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
+    axes = (ax1, ax2)
+    ax1.plot(s.time, s.x0, label="x0")
+    ax1.plot(s.time, s.x1, label="x1")
+    ax1.plot(s.time, s.x2, label="x2")
+
+    ax2.plot(s.x0, s.x1, label="x1 ~ x0")
+    ax2.plot(s.x0, s.x2, label="x2 ~ x0")
+    ax2.plot(s.x1, s.x2, label="x2 ~ x1")
+
+    ax1.set_title("Lorenz Time Course")
+    ax1.set_xlabel("time")
+    ax2.set_title("Lorenz State Space")
+    ax2.set_xlabel("value")
+
+    for ax in axes:
+        ax.legend()
+        ax.set_ylabel("value")
+
+    plt.show()
+
+    return fig
+
+
+if __name__ == "__main__":
+
+    # simulate
+    time = np.arange(0, 20, 0.001)
+    x0 = [1, 1, 1]
+    xids = ['x0', 'x1', 'x2']
+    X = odeint(lorenz_dxdt, x0, time)
+
+    # create data frame
+    s = pd.DataFrame(X, columns=xids)
+    s['time'] = time
+    print(s.head())
+
+    # plot attractor
+    lorenz_plot(s)
